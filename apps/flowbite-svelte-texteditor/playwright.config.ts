c40fa428023939b3adbd3ba6b1030074ca4697de
Joseph Robinson <smartech56@gmail.com>
2026-05-24 03:00:03 -0700

    Fix: Texteditor: Tests failing

--- apps/flowbite-svelte-texteditor/playwright.config.ts
+++ apps/flowbite-svelte-texteditor/playwright.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   webServer: {
-    command: 'npm run build && npm run preview',
-    port: 4173
+    command: 'npm run dev -- --host 127.0.0.1 --port 4173',
+    port: 4173,
+    timeout: 180 * 1000,
+    reuseExistingServer: !process.env.CI
   },
   testDir: 'e2e'
 });
